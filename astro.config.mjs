bbbe512075f6e8521c858c0dde57486cfe1cffeb
Euler Toledo <eulertoledito3@gmail.com>
2024-04-15 19:52:29 -0500

    tienda mas mapeo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   /*image: '/preview-image.png',*/
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), db()]
 });
---
+++ src/components/tienda/Zapatos.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+//import datos from '../../../data.json';
+//const { calzadoDama } = datos
+const Zapatos = ( { damas, caballeros }) => {
+  return (
+    <div>
+      este es mi componente zapato
+      <h1>fama2</h1>
+      <img src="/images/calzados/taco1.png" alt="" />
+      <div className="w-full grid grid-cols-4 ">
+      {damas.map((dato) => (
+          <div className="w-full flex" key={dato.id}>
+              <div className="w-6/12"><img className="w-4/5" src={dato.url} alt={dato.descripcion} /></div>
+              <div className="w-6/12">
+                <h2>{dato.title}</h2>
+                <p>{dato.descripcion}</p>
+                <p>{dato.precio}</p>
+              </div> 
+          </div>
+        ))
+      }
+      </div>
+    </div>
+  )
+}
+
+export default Zapatos
